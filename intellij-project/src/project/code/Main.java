3a8e95985c1eb33c7f2575a7f3c361905fb6b3cf
Jonathan Harper <jonathanvharper@gmail.com>
2018-03-15 12:06:06 +0000

    Pushing

--- intellij-project/src/project/code/Main.java
+++ intellij-project/src/project/code/Main.java
@@ -6,8 +6,12 @@ import it.polito.appeal.traci.SumoTraciConnection;
 
 public class Main {
 
+    public static final String NORMAL_CONFIG = "src/project/configuration_files/config.cfg";
+    public static final String LITE_CONFIG = "src/project/configuration_files/config_lite.cfg";
+
     static String sumo_bin = "D:/Program Files/SUMO/bin/sumo-gui.exe";
-    static final String config_file = "src/de/tudresden/test/sim/config.sumo.cfg";
+    static final String config_file = NORMAL_CONFIG;
+
 
     public static void main(String[] args) {
 
@@ -26,11 +30,7 @@ public class Main {
             conn.do_timestep();
 
             for(int i=0; i<3600; i++){
-
-                //current simulation time
-                int simtime = (int) conn.do_job_get(Simulation.getCurrentTime());
-
-                conn.do_job_set(Vehicle.add("veh"+(i+1), "car", "s1", simtime, 0, 13.8, (byte) 1));
+                System.out.println("hello");
                 conn.do_timestep();
             }
 
