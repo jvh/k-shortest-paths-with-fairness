65fa99d8cbe41a4878c415c874ab1d1907c7c1eb
Jonathan Harper <jonathanvharper@gmail.com>
2018-03-15 23:32:37 +0000

    Got small_manhattan working with 1 vehicle and a set route

--- intellij-project/src/project/small_manhattan/code/MainSmallManhattan.java
+++ intellij-project/src/project/small_manhattan/code/MainSmallManhattan.java
@@ -1,15 +1,16 @@
-package project.southampton.code;
+package project.small_manhattan.code;
 
+import de.tudresden.sumo.cmd.Vehicle;
 import it.polito.appeal.traci.SumoTraciConnection;
 
 public class MainSmallManhattan {
 
-    public static final String NORMAL_CONFIG = "src/project/configuration_files/config.cfg";
+    public static final String NORMAL_CONFIG = "src/project/small_manhattan/configuration_files/config.cfg";
     public static final String LITE_CONFIG = "src/project/configuration_files/config_lite.cfg";
     public static final String TEST_CONFIG = "src/project/configuration_files/config_test.cfg";
 
     static String sumo_bin = "D:/Program Files/SUMO/bin/sumo-gui.exe";
-    static final String config_file = TEST_CONFIG;
+    static final String config_file = NORMAL_CONFIG;
 
 
     public static void main(String[] args) {
@@ -28,7 +29,12 @@ public class MainSmallManhattan {
             //load routes and initialize the simulation
             conn.do_timestep();
 
+
+            conn.do_job_set(Vehicle.add("veh", "car", "s1", 0, 2, 1, (byte) 0));
+            System.out.println(conn.do_job_get(Vehicle.getBestLanes("veh")));
 //            Collection<Vehicle> vehicles = conn.
+
+
 
             for(int i=0; i<3600; i++){
                 conn.do_timestep();
