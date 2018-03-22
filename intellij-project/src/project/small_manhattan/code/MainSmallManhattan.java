28344f31744b5f4778e89bbdb7c467942b3e789b
Jonathan Harper <jonathanvharper@gmail.com>
2018-03-22 01:10:30 +0000

    Cleared up code + added some small features (zoom, current time, vehicle setting edge effort based on global edge effort)

--- intellij-project/src/project/small_manhattan/code/MainSmallManhattan.java
+++ intellij-project/src/project/small_manhattan/code/MainSmallManhattan.java
@@ -1,6 +1,7 @@
 package project.small_manhattan.code;
 
 import de.tudresden.sumo.cmd.*;
+import de.tudresden.sumo.util.Sumo;
 import de.tudresden.ws.Traci;
 import de.tudresden.ws.container.SumoStringList;
 import it.polito.appeal.traci.SumoTraciConnection;
@@ -16,108 +17,79 @@ public class MainSmallManhattan {
     //Simulation start and end times
     public static final int START_TIME = 0;
     public static final int END_TIME = 100000;
+    //Zoom factor for GUI
+    public static final int ZOOM_FACTOR = 12;
 
     public static final String SUMO_BIN = "D:/Program Files/SUMO/bin/sumo-gui.exe";
     public static final String CONFIG_FILE = NORMAL_CONFIG;
 
+    public static final SumoTraciConnection SUMO_CONN = new SumoTraciConnection(SUMO_BIN, CONFIG_FILE);
 
     public static void main(String[] args) {
+        MainSmallManhattan msm = new MainSmallManhattan();
+        SumoTraciConnection conn = msm.SUMO_CONN;
 
-        //start Simulation
-        SumoTraciConnection conn = new SumoTraciConnection(SUMO_BIN, CONFIG_FILE);
-
-        //set some options
-        conn.addOption("step-length", "0.1"); //timestep 100 ms
-//        conn.addOption("device.rerouting.probability", "1.0");
-//        conn.addOption("routing-algorithm", "dijkstra");
+        //Changing the timestep to be 0.1s
+        conn.addOption("step-length", "0.1");
+        conn.addOption("device.rerouting.probability", "1.0");
+        conn.addOption("routing-algorithm", "dijkstra");
 
         try{
-
-            //start TraCI
+            //Start Traci
             conn.runServer();
-
-
-
-            //load routes and initialize the simulation
+            //Initialise the simulation & load routers + vehicles
             conn.do_timestep();
-            Traci traci = new Traci();
 
-
-
-            //Printing out all the edges in the simulation
-            SumoStringList edges = (SumoStringList) conn.do_job_get(Edge.getIDList());
-            for(String edge: edges) {
-//                System.out.println("edge " + edge);
-            }
-
+            //Creating a starting node
             SumoStringList listExample = new SumoStringList();
             ArrayList<String> edgeList = new ArrayList<>();
-
-            edgeList.add("452322756#0");
-//            edgeList.add("452322756#1");
-//            edgeList.add("452322756#2");
+            edgeList.add("46538375#6");
             listExample.addAll(edgeList);
+            conn.do_job_set(Route.add("startNode", listExample));
 
+            //Adding in vehicle
+            conn.do_job_set(Vehicle.add("testVeh", "car", "startNode", 0, 1, 1, (byte) 0));
 
+            //GUI tracking vehicle and zoom
+            conn.do_job_set(Gui.trackVehicle("View #0", "testVeh"));
+            conn.do_job_set(Gui.setZoom("View #0", (Double.parseDouble(conn.do_job_get(Gui.getZoom("View #0")).toString()))*ZOOM_FACTOR));
 
-            conn.do_job_set(Route.add("test", listExample));
+            //Changing the target of the vehicle to another edge
+            conn.do_job_set(Vehicle.changeTarget("testVeh", "569345537#2"));
 
-//            System.out.println(conn.do_job_get(Route.getEdges("test")));
-
-//            conn.do_job_set(Vehicle.add("veh", "car", "test", 0, 2, 1, (byte) 0));
-
-            conn.do_job_set(Vehicle.add("veh2", "car", "s1", 0, 1, 1, (byte) 0));
-
-            conn.do_job_set(Gui.trackVehicle("View #0", "veh2"));
-
-            conn.do_job_set(Vehicle.changeTarget("veh2", "569345537#2"));
-
-            //Across
-//            conn.do_job_set(Vehicle.setEffort("veh2", 0, END_TIME, "46538375#8", 9));
-//
-//            //Down
-//            conn.do_job_set(Vehicle.setEffort("veh2", 0, END_TIME, "196116976#7", 3));
-//            conn.do_job_set(Vehicle.setEffort("veh2", 0, END_TIME, "196116976#9", 2));
-//            conn.do_job_set(Vehicle.setEffort("veh2", 0, END_TIME, "420908138#1", 7));
-
-
-            conn.do_job_set(Edge.setEffort("46538375#8", 5));
+            //Setting efforts of edges
+            conn.do_job_set(Edge.setEffort("46538375#8", 1));
             conn.do_job_set(Edge.setEffort("196116976#7", 2));
-            
 
 
-//            conn.do_job_set(Edge.setEffort("196116976#7", 1));
+            //Running for the duration of the simulation
+            for(int i = START_TIME; i < END_TIME; i++){
+                if(i == 10) {
+                    msm.testVehicleSetEffort(conn, "testVeh", "46538375#8");
 
-//            conn.do_job_set(Vehicle.rerouteEffort("veh2"));
-            System.out.println("Current time: " + conn.do_job_get(Simulation.getCurrentTime()));
-
-//            System.out.println("Effort " + conn.do_job_get(Edge.getEffort("46538375", 0)));
-
-//            conn.do_job_set(Vehicle.rerouteEffort("veh2"));
-
-//            System.out.println(conn.do_job_get(Vehicle.getBestLanes("veh")));
-//            Collection<Vehicle> vehicles = conn.
-
-
-
-            for(int i=START_TIME; i<END_TIME; i++){
-
-                if(i==10) {
-                    conn.do_job_set(Vehicle.rerouteEffort("veh2"));
-                    System.out.println("Vehicle effort veh2: " + conn.do_job_get(Vehicle.getEffort("veh2", i, "196116976#7")));
-
+                    conn.do_job_set(Vehicle.rerouteEffort("testVeh"));
                 }
 
                 conn.do_timestep();
             }
-
-//            System.out.println(conn.do_job_get(Edge.getEffort("196116976#7", 10)));
-
             //stop TraCI
             conn.close();
 
-        }catch(Exception ex){ex.printStackTrace();}
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    //Returns the current time of the simulation in ms
+    public static int getCurrentTime() throws Exception {
+      return Integer.parseInt(SUMO_CONN.do_job_get(Simulation.getCurrentTime()).toString());
+    }
+
+    private void testVehicleSetEffort(SumoTraciConnection conn, String vehID, String edgeID) throws Exception {
+        double edgeEffortDouble = Double.parseDouble(conn.do_job_get(Edge.getEffort(edgeID, getCurrentTime())).toString());
+        conn.do_job_set(Vehicle.setEffort(vehID, START_TIME, END_TIME, edgeID, edgeEffortDouble));
+
+        System.out.println("Vehicle effort for " + vehID + " on edge " + edgeID + " at time " + getCurrentTime() + ": " + conn.do_job_get(Vehicle.getEffort(vehID, getCurrentTime(), edgeID)));
     }
 
 }
