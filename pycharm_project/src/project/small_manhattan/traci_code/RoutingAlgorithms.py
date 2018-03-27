5b6d6a2c8d3996bd05f72cf0cb380efbbb47fa1e
Jonathan Harper <jonathanvharper@gmail.com>
2018-03-27 23:55:24 +0100

    Added pydocs

--- pycharm_project/src/project/small_manhattan/traci_code/RoutingAlgorithms.py
+++ pycharm_project/src/project/small_manhattan/traci_code/RoutingAlgorithms.py
@@ -9,7 +9,13 @@ TESTING_NUMBER = 1
 class Testing:
 
     def testVehicleSetEffort(self, vehID, edgeID):
-        """ Sets the effort of a vehicle for a given edge """
+        """
+        Sets the effort of a vehicle for a given edge
+
+        Args:
+            vehID (str): The ID of the vehicle
+            edgeID (str): The ID of the edge
+        """
         # Get the current global effort for the edge
         edgeEffortDouble = traci.edge.getEffort(edgeID, sumo.Main.getCurrentTime())
         # Sets the effort to this particular vehicle
@@ -20,74 +26,55 @@ class Testing:
                       traci.vehicle.getEffort(vehID, sumo.Main.getCurrentTime(), edgeID)))
 
     def getIncomingEdges(self, edgeID):
-        """ Returns a list of all of the incoming edges to the specified edge """
+        """
+        Returns a list of all of the incoming edges to the specified edge
+
+        Args:
+            edgeID (str): The identification of the edge to collect incoming edges
+        Returns:
+            incEdgeList (str[]): The edges incoming to edgeID
+        """
         incEdgeList = []
         for edgeInc in sumo.net.getEdge(edgeID).getIncoming():
             incEdgeList.append(edgeInc.getID())
         return incEdgeList
 
-    # def recursiveIncomingEdges(self, edgeID, limit=0, edgeList=[]):
-    #     """ Recurses down the incoming edges of the edge defined to return a list of all of the incoming edges from x edges
-    #      away """
-    #     # CHECK IF THE EDGE IS AT THE EDGE OF THE MAP (USING SUMOLIB)
-    #     # ALSO, USE ++variable RATHER THAN variable += 1
-    #     if limit < 2:
-    #         edges = self.getIncomingEdges(edgeID)
-    #         for edge in edges:
-    #
-    #     else:
-    #         return edgeList
 
-    def recursiveIncomingEdgesNew(self, edgeID, firstTime, limit=0, edgeList=[], eg=["test"], edgeOrderList={}):
-        """ Recurses down the incoming edges of the edge defined to return a list of all of the incoming edges from x edges
-         away """
-        # CHECK IF THE EDGE IS AT THE EDGE OF THE MAP (USING SUMOLIB)
-        # ALSO, USE ++variable RATHER THAN variable += 1
-        if eg or limit < 2:
-            if eg == ["test"]:
-                eg.remove("test")
-            if edgeID != "" and limit < 2:
+    def recursiveIncomingEdges(self, edgeID, firstTime=False, edgeList=[], edgesToSearch=["placeholder"],
+                               edgeOrderList={}, finished=False):
+        """
+        Recurses down the incoming edges of the edge defined to return a list of all of the incoming edges from x edges
+        away
 
-
-
-                edges = self.getIncomingEdges(edgeID)
-                for edge in edges:
-                    print("edge {} and edges {}".format(edge, edges))
-                    edgeList.append(edge)
-                    eg.append(edge)
-            # for e in eg:
-            explore = eg.pop()
-            if limit < 2:
-                limit += 1
-                return self.recursiveIncomingEdges(explore, limit, edgeList, eg)
-            else:
-                return self.recursiveIncomingEdges(explore, 0, eg)
-        else:
-            return edgeList
-
-    def recursiveIncomingEdges(self, edgeID, firstTime=False, edgeList=[], edgesToSearch=["placeholder"], edgeOrderList={}, finished=False):
-        """ Recurses down the incoming edges of the edge defined to return a list of all of the incoming edges from x edges
-         away """
-        # CHECK IF THE EDGE IS AT THE EDGE OF THE MAP (USING SUMOLIB)
-        # ALSO, USE ++variable RATHER THAN variable += 1
-        if (edgesToSearch or edgeOrderList[edgeID] != sumo.EDGE_RECURSIONS) and not finished:
+        Args:
+            edgeID (str): The edge in which to find the incoming edges
+            firstTime (bool): If this is the first time running the module
+            edgeList (str[]): The list holding all of the incoming edges from the original node up to
+                MAX_EDGE_RECURSION_RANGE
+            edgesToSearch (str[]): The list specifying the edges which are to be searched for incoming edges
+            finished (bool): Specifies if the function has finished (all edges searched)
+        Returns:
+            str[]: The edges incoming to the initial edge up to MAX_EDGE_RECURSION_RANGE
+        """
+        if (edgesToSearch or edgeOrderList[edgeID] != sumo.MAX_EDGE_RECURSIONS_RANGE) and not finished:
             if firstTime:
                 edgeOrderList[edgeID] = 0
+                # Remove the placeholder value from the edgesToSearch stack
                 edgesToSearch.remove("placeholder")
 
+            # Gets the immediate incoming edges for edgeID
             edges = self.getIncomingEdges(edgeID)
             for edge in edges:
-                print("edge {} and edges {}".format(edge, edges))
                 edgeList.append(edge)
+                # The new edge has range of edgeOrderList[edgeID] + 1 away from the original edge
                 edgeOrderList[edge] = edgeOrderList[edgeID] + 1
-                if edgeOrderList[edge] != sumo.EDGE_RECURSIONS:
+                # If the range of the edge isn't the maximum range
+                if edgeOrderList[edge] != sumo.MAX_EDGE_RECURSIONS_RANGE:
                     edgesToSearch.append(edge)
-
-            print("NOW HERE {}".format(edgeOrderList))
 
             # There are still more edges to search
             if edgesToSearch:
-                # Pop
+                # Pop from the stack the edge to search
                 edgeToTest = edgesToSearch.pop()
                 finished = False
             # If there are no more edges to search
@@ -95,107 +82,23 @@ class Testing:
                 finished = True
                 # Prevent an error as there are no more edges to test
                 edgeToTest = edgeID
-            return self.recursiveIncomingEdges(edgeToTest, finished=finished, edgeList=edgeList, edgesToSearch=edgesToSearch, edgeOrderList=edgeOrderList)
+            # Recursively call with the new edge
+            return self.recursiveIncomingEdges(edgeToTest, finished=finished, edgeList=edgeList,
+                                               edgesToSearch=edgesToSearch, edgeOrderList=edgeOrderList)
         else:
             return edgeList
 
     def getMultiIncomingEdges(self, edgeID):
-        """ User friendly approach to getting the number of incoming edges from an edge """
+        """
+        User friendly approach to getting the number of incoming edges from an edge (the recursiveIncomingEdge()
+        function)
+
+        Args:
+            edgeID (str): The initial edge
+        Returns:
+            str[]: The edges incoming to the initial edge up to MAX_EDGE_RECURSION_RANGE
+        """
         return self.recursiveIncomingEdges(edgeID, firstTime=True)
-
-    def recursiveIncomingEdgeskajdl(self, edgeID="null", limit=0, edgeList=[], eg=[]):
-        """ Recurses down the incoming edges of the edge defined to return a list of all of the incoming edges from x edges
-         away """
-        # CHECK IF THE EDGE IS AT THE EDGE OF THE MAP (USING SUMOLIB)
-        # ALSO, USE ++variable RATHER THAN variable += 1
-        if limit < 2:
-            edges = self.getIncomingEdges(edgeID)
-            if not eg:
-                eg = edges
-            for edge in eg:
-                print("edge {} and edges {}".format(edge, eg))
-                edgeList.append(edge)
-                eg.remove(edge)
-                limit += 1
-                return self.recursiveIncomingEdges(edge, limit, edgeList, eg)
-        else:
-            if eg:
-                return self.recursiveIncomingEdges(limit=0, edgeList=edgeList, edgesToSearch=eg)
-            else:
-                return edgeList
-
-    def recursiveIncomingEdges4(self, edgeID="null", limit=0, edgeList=[], eg=[]):
-        """ Recurses down the incoming edges of the edge defined to return a list of all of the incoming edges from x edges
-         away """
-        # CHECK IF THE EDGE IS AT THE EDGE OF THE MAP (USING SUMOLIB)
-        # ALSO, USE ++variable RATHER THAN variable += 1
-        if limit < 2:
-            edges = self.getIncomingEdges(edgeID)
-            if not eg:
-                eg = edges
-            for edge in eg:
-                print("edge {} and edges {}".format(edge, eg))
-                edgeList.append(edge)
-                eg.remove(edge)
-                limit += 1
-                return self.recursiveIncomingEdges(edge, limit, edgeList, eg)
-        else:
-            if eg:
-                return self.recursiveIncomingEdges(limit=0, edgeList=edgeList, edgesToSearch=eg)
-            else:
-                return edgeList
-
-        def recursiveIncomingEdges3(self, edgeID, limit=0, edgeList=[]):
-            """ Recurses down the incoming edges of the edge defined to return a list of all of the incoming edges from x edges
-             away """
-            # CHECK IF THE EDGE IS AT THE EDGE OF THE MAP (USING SUMOLIB)
-            # ALSO, USE ++variable RATHER THAN variable += 1
-            if limit < 2:
-                edges = self.getIncomingEdges(edgeID)
-                for edge in edges:
-                    print("edge {} and edges {}".format(edge, edges))
-                    edgeList.append(edge)
-                    limit += 1
-                    return self.recursiveIncomingEdges(edge, limit, edgeList)
-            else:
-                return edgeList
-
-        def recursiveIncomingEdges2(self, edgeID, internalCounter=0, num=0, limit=0, edgeList=[], oldList=[]):
-            """ Recurses down the incoming edges of the edge defined to return a list of all of the incoming edges from x edges
-             away """
-            # CHECK IF THE EDGE IS AT THE EDGE OF THE MAP (USING SUMOLIB)
-            # ALSO, USE ++variable RATHER THAN variable += 1
-            if limit <= self.NUMBER_OF_RECURSIONS:
-
-                edges = self.getIncomingEdges(edgeID)
-                list2 = []
-                if not oldList:
-                    limit += 1
-                    oldList = edges
-                    for s in oldList:
-                        oldList.remove(s)
-                        return self.recursiveIncomingEdges(s, internalCounter, num, limit, edgeList, oldList)
-                else:
-                    for s in oldList:
-                        oldList.remove(s)
-                        return self.recursiveIncomingEdges(s, internalCounter, num, limit, edgeList, oldList)
-
-                        # if num == 0:
-                        #     numberOfEdges = len(edges)
-                        #     num = numberOfEdges
-                        # for edge in edges:
-                        #     print("edge {} and edges {}".format(edge, edges))
-                        #     edgeList.append(edge)
-                        #     # if internalCounter <= num:
-                        #     if internalCounter <= self.NUMBER_OF_RECURSIONS:
-                        #         internalCounter += 1
-                        #         return self.recursiveIncomingEdges(edge, internalCounter, num, limit, edgeList, list2)
-                        # limit += 1
-                        # return self.recursiveIncomingEdges(edge, 0, 0, limit, edgeList)
-                        # return self.recursiveIncomingEdges(edge, 0, 0, limit, edgeList)
-                        # return
-            else:
-                return edgeList
 
     def test1Before(self):
         # Adding vehicle and associated route
@@ -245,9 +148,7 @@ class Testing:
             # Prints the incoming edges for 196116976#7
             print(self.getIncomingEdges("196116976#7"))
 
-            # print("Recursive edges for 196116976#7 are: {}".format(self.recursiveIncomingEdges("196116976#7", firstTime=True)))
             print("Recursive edges for 511924978#1 are: {}".format(self.getMultiIncomingEdges("511924978#1")))
-
 
             time.sleep(2)
 
--- pycharm_project/src/project/small_manhattan/traci_code/SumoConnection.py
+++ pycharm_project/src/project/small_manhattan/traci_code/SumoConnection.py
@@ -3,21 +3,25 @@ import sumolib
 import os
 from src.project.small_manhattan.traci_code import RoutingAlgorithms as routing
 
+# Configuration files
 NORMAL_CONFIG = "project/small_manhattan/configuration_files/normal/config.cfg"
 TEST_CONFIG = "project/small_manhattan/configuration_files/testing/config_test.cfg"
 NET_FILE_TESTING = "D:/Nina/Dropbox/UNIVERSITY/YEAR 3/COMP3200 - 3rd Year Individual Project/sumo-project/" \
                   "pycharm_project/src/project/small_manhattan/configuration_files/testing/small_manhattan.net.xml"
 SUMO_BINARY = "D:/Program Files/SUMO/bin/sumo-gui.exe"
 
+# SUMO settings
 START_TIME = 0
 END_TIME = 10000
 ZOOM_FACTOR = 12
 
-# This specifies the number of edges away from the current edge to search for vehicles
-EDGE_RECURSIONS = 3
+# This specifies the number of incoming edges away (the range) from the original edge to search
+MAX_EDGE_RECURSIONS_RANGE = 3
 
+# Specifies if testing should take place
 TESTING = True
 
+# Passes the network file into sumolib for analysis and use
 net = sumolib.net.readNet(NET_FILE_TESTING)
 
 @staticmethod
@@ -31,9 +35,11 @@ class Main:
         return traci.simulation.getCurrentTime()
 
     def run(self):
+        """ Starts the simulation """
+
         # Defines the command to start SUMO with
         sumo = [SUMO_BINARY, '-c', TEST_CONFIG, '--step-length', '0.1']
-        # Establish the Traci connection and run sumo cmd
+        # Establish the Traci connection and run SUMO command
         traci.start(sumo)
 
         test = routing.Testing()
